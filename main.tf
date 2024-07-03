42be3ca7a7dde6df0c7602cd1e1227b522f1beea
Kashif Sadiq <79781629+KashifSad@users.noreply.github.com>
2024-07-03 22:50:14 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "me-south-1" # Change to your desired region
+}
+
+
 resource "aws_instance" "web" {
   ami           = "ami-087f3ec3fdda67295"
   instance_type = "t3.micro"
